182b2e117b7dcb26217c065b259edb57cf692f34
Shrikant Pandey <shrikantpandeymnnit2015@gmail.com>
2018-06-26 21:45:46 +0530

    A. Watermelon

---
+++ Ideone.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int w = sc.nextInt();
+		if (w%2==0 & w!=2)
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+
+	}
+}
